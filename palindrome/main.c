3fb2f6b0b4ecbf92ad7c24a2f04970e33d9b5446
arons121 <arons121@morris.umn.edu>
2019-09-19 11:18:43 -0500

    Fixed leaks in main, test and palindrome.c
    Deallocated memory in each of the files in order to fix the leaks.
    
    Co-authored-by: Colt Dahl <dahl1173@morris.umn.edu>

--- palindrome/main.c
+++ palindrome/main.c
@@ -15,13 +15,21 @@ int main(int argc, char *argv[]) {
   // and returns it in the buffer passed as the first argument
   // (line here). If there's no more input to read from, it
   // returns NULL.
+ // char* palindromeline = palindrome(line);
+
   while (fgets(line, size, stdin) != NULL) {
     // This crazy line removes the '\n' at the end of the input line.
     // Without it, nothing will ever be a palindrome, unless it happens
     // to somehow start with a '\n'.
-    line[strcspn(line, "\n")] = '\0';
-    printf("Is the string <%s> a palindrome? %s\n", line, palindrome(line));
-  }
+    char* palindromeline;
 
+    line[strcspn(line, "\n")] = '\0';
+    palindromeline = palindrome(line);
+    printf("Is the string <%s> a palindrome? %s\n", line, palindromeline);
+    free(palindromeline);
+  
+  }
+  
+  
   free(line);
 }
--- palindrome/palindrome_test.cpp
+++ palindrome/palindrome_test.cpp
@@ -3,11 +3,15 @@
 #include "palindrome.h"
 
 void is_palindrome(char const *str) {
-  ASSERT_STREQ(palindrome(str), "Yes");
+  char* palindromestr = palindrome(str);
+  ASSERT_STREQ(palindromestr, "Yes");
+  free(palindromestr);
 }
 
 void not_palindrome(char const *str) {
-  ASSERT_STREQ(palindrome(str), "No");
+   char* palindromestr = palindrome(str);
+  ASSERT_STREQ(palindromestr, "No");
+  free(palindromestr);
 }
 
 TEST(Palindrome, HandlesEmptyString) {
